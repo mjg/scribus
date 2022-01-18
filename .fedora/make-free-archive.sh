c96f04adc77236cc78bb227b9f60856f630e1240
Michael J Gruber <mjg@fedoraproject.org>
2022-01-18 11:35:34 +0100

    import make-free script from dist-git

---
+++ .fedora/make-free-archive.sh
@@ -0,0 +1,56 @@
+#! /bin/sh
+
+if [ -z "$1" ]; then
+    echo "version required"
+    exit 1
+fi
+
+version=$1
+nv=scribus-${version}
+
+archive=${nv}.tar.xz
+freearchive=${nv}-free.tar.xz
+
+echo "Verify if source available. Download may be needed"
+[ -f ${archive} ] || curl -OL http://downloads.sourceforge.net/scribus/scribus-${version}.tar.xz
+
+echo "Extracting sources ..."
+rm -rf ${nv}
+tar -xJf $archive
+
+pushd ${nv}
+
+# remove docs
+rm -r doc
+sed -i '/add_subdirectory(doc)/d' CMakeLists.txt
+echo "doc folder removed"
+
+# remove non-free profiles
+rm resources/profiles/{GenericCMYK.icm,GenericCMYK.txt,sRGB.icm,srgb.license}
+sed -i '/GenericCMYK/d' resources/profiles/CMakeLists.txt
+sed -i '/sRGB.icm/d' resources/profiles/CMakeLists.txt
+sed -i '/srgb.license/d' resources/profiles/CMakeLists.txt
+echo "Non-free profiles removed"
+
+# remove non-free swatches
+rm resources/swatches/Galaxy_*
+rm resources/swatches/Resene_*
+rm resources/swatches/Federal_Identity_Program.xml
+echo "Non-free swatches removed"
+
+# Remove non-free (CC-BY-SA-NC) dot files
+rm resources/editorconfig/dot.*
+sed -i '/dot.svg/d' resources/editorconfig/CMakeLists.txt
+echo "Non-free dot files removed"
+
+# Remove non-free dicts files
+rm -r resources/dicts
+sed -i '/dicts/d' CMakeLists.txt
+echo "Non-free dicts removed"
+
+popd
+
+echo "Creating sources ..."
+tar cf - ${nv} | xz > ${freearchive}
+echo "Source free archives created"
+echo "Task completed"
